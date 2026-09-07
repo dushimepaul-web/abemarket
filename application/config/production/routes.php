3f69a99fd72270678e9895bd0d583c4a954b8bae
dushimepaul-web <dushimepaul51@gmail.com>
2026-09-07 18:46:11 +0200

    Sync production routes with development routes to prevent 404 on /shop and others

--- application/config/production/routes.php
+++ application/config/production/routes.php
@@ -1,30 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'Home';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING (PRODUCTION)
+| -------------------------------------------------------------------------
+| Copie complète et identique du fichier de routes pour la production.
+*/
 
-// Copie exacte des routes de développement pour la production
-$route['shop'] = 'home/shop';
-$route['home'] = 'home/index';
-$route['about'] = 'home/about';
-$route['blog'] = 'home/blog';
-$route['offres'] = 'home/offres';
-$route['faq'] = 'home/faq';
-$route['contact'] = 'home/contact';
-$route['cart'] = 'home/cart';
-$route['checkout'] = 'home/checkout';
-$route['checkout/process'] = 'home/processOrder';
-$route['payment/pending/(:any)'] = 'home/payment_pending/$1';
-$route['payment/success/(:any)'] = 'Payment/Payment/success/$1';
-$route['payment/submit-reference'] = 'home/submit_payment_reference';
-$route['wishlist'] = 'home/wishlist';
-$route['privacy-policy'] = 'home/privacy_policy';
-$route['sellers'] = 'home/sellers';
-$route['search'] = 'home/search';
-
-$route['category/(:any)'] = 'home/category/$1';
-$route['product/(:any)'] = 'home/product/$1';
-$route['seller/(:any)'] = 'home/seller/$1';
-$route['order-success/(:any)'] = 'home/order_success/$1';
+include(APPPATH.'config/development/routes.php');
