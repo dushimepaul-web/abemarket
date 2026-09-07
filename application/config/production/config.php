e0166af992883341cce7a03f81c89b5e84f052af
dushimepaul-web <dushimepaul51@gmail.com>
2026-09-07 18:51:29 +0200

    dsk

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on their own.
 |
 */
-$config['base_url'] = 'https://abemarket.com/';
+$config['base_url'] = 'https://market.abe.bi/';
 
 /*
 |--------------------------------------------------------------------------
