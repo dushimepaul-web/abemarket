f61089432cce66a8622a96ebb1d08cec444f189f
dushimepaul-web <dushimepaul51@gmail.com>
2026-09-07 18:58:00 +0200

    dsk

--- application/config/production/routes.php
+++ application/config/production/routes.php
@@ -1,11 +1,4 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING (PRODUCTION)
-| -------------------------------------------------------------------------
-| Copie complète et identique du fichier de routes pour la production.
-*/
-
 include(APPPATH.'config/development/routes.php');
