3672e0b5d2067dfdfbd7beaad084aa0d74ca41f9
dushimepaul-web <dushimepaul51@gmail.com>
2026-09-07 18:43:36 +0200

    dsk

--- application/config/production/routes.php
+++ application/config/production/routes.php
@@ -1,54 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	https://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
-*/
-$route['default_controller'] = '';
+$route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Copie exacte des routes de développement pour la production
+$route['shop'] = 'home/shop';
+$route['home'] = 'home/index';
+$route['about'] = 'home/about';
+$route['blog'] = 'home/blog';
+$route['offres'] = 'home/offres';
+$route['faq'] = 'home/faq';
+$route['contact'] = 'home/contact';
+$route['cart'] = 'home/cart';
+$route['checkout'] = 'home/checkout';
+$route['checkout/process'] = 'home/processOrder';
+$route['payment/pending/(:any)'] = 'home/payment_pending/$1';
+$route['payment/success/(:any)'] = 'Payment/Payment/success/$1';
+$route['payment/submit-reference'] = 'home/submit_payment_reference';
+$route['wishlist'] = 'home/wishlist';
+$route['privacy-policy'] = 'home/privacy_policy';
+$route['sellers'] = 'home/sellers';
+$route['search'] = 'home/search';
+
+$route['category/(:any)'] = 'home/category/$1';
+$route['product/(:any)'] = 'home/product/$1';
+$route['seller/(:any)'] = 'home/seller/$1';
+$route['order-success/(:any)'] = 'home/order_success/$1';
