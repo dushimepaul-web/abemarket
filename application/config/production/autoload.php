15cbf611ebb92b605c3ecbca3dd7ac61b194dcf4
dushimepaul-web <dushimepaul51@gmail.com>
2026-09-07 19:24:48 +0200

    dsk

--- application/config/production/autoload.php
+++ application/config/production/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'email', 'session', 'upload', 'user_agent', 'form_validation', 'Mailer');
+$autoload['libraries'] = array('database', 'email', 'session', 'upload', 'user_agent', 'form_validation', 'Cpanel_email_lib');
 
 /*
 | -------------------------------------------------------------------
